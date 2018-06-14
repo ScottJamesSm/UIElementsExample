106a589471fa401bede9a1844d6ac8e38da008ec
Scott James Smith <slocknog1982@gmail.com>
2018-06-14 13:52:33 -0400

    added textbox and more UI elements

--- app/src/main/java/com/example/scottsmith/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/scottsmith/uielementsexamples/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.scottsmith.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView6);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        textView.setText("This text is changed in the on create");
+
+        setListener();
     }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+            }
+        });
+            }
+
 }
